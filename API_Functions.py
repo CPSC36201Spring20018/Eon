7d490d00307564c4ff0be8928718b701115bf2bb
SeaLabEZF <sean.wulwick@gmail.com>
2018-04-23 16:37:23 -0700

    edited api functions to be more inline with what they are used for, renamed file API_Functions.py, also removed unneeded API key.txt file

--- API_Functions.py
+++ API_Functions.py
@@ -3,23 +3,25 @@ from weather import Weather, Unit
 def threeDayForecast(loc):
     weather = Weather(unit=Unit.FAHRENHEIT)
     location = weather.lookup_by_location(loc)
-    condition = location.condition
     forecasts = location.forecast
-    for i in range(3):
-        print(forecasts[i].day)
-        print(forecasts[i].date)
-        print(forecasts[i].high)
-        print()
+    return forecasts
 
 def locationTemperature(loc):
     weather = Weather(unit=Unit.FAHRENHEIT)
     location = weather.lookup_by_location(loc)
     condition = location.condition
     temperature = condition.temp
-    print(temperature)
+    return temperature
 
-loca = 'Long Beach,CA'
-threeDayForecast(loca)
+loca = 'Fullerton,CA'
+print("it is currently ", locationTemperature(loca), " in ", loca)
+print()
+forecast = threeDayForecast(loca)
+for i in range(1,4):
+    print(forecast[i].day)
+    print(forecast[i].high)
+    print(forecast[i].text)
+    print()
 loca = 'New York,NY'
-locationTemperature(loca)
+print("it is currently ", locationTemperature(loca), " in ", loca)
 
