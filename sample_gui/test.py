886869d8ec5a3afd472b166cc119486ae5ebbeea
Erine Estrella <oscreader@OSC>
2018-05-03 22:10:31 -0600

    updated overall GUI.

--- sample_gui/test.py
+++ sample_gui/test.py
@@ -11,6 +11,7 @@ from tkcalendar import Calendar, DateEntry
 import datetime
 import requests
 import json
+
 #import Tkinter as tk     # python 2
 #import tkFont as tkfont  # python 2
 
@@ -24,33 +25,27 @@ class SampleApp(tk.Tk):
         tk.Tk.__init__(self, *args, **kwargs)
      
         # Fonts defined here#
-        self.temp_font = tkfont.Font(family='Ariel', size=35, weight="bold", slant="italic")
-        self.time_font = tkfont.Font(family='Ariel', size=10)
-        self.day_font = tkfont.Font(family='Ariel', size=15)
+        self.temp_font = tkfont.Font(family= 'Calibri', size=40)
+        self.time_font = tkfont.Font(family='Calibri', size=10)
+        self.day_font = tkfont.Font(family='Calibri', size=15)
 
         # the container is where we'll stack a bunch of frames
         # on top of each other, then the one we want visible
         # will be raised above the others
         self.title("Den")
-        w = self.winfo_screenwidth()
-        h = self.winfo_screenheight()
-        self.geometry("%dx%d+0+0"%(w,h))
-      
- 
-        spaceImage = tk.PhotoImage(file = "space.gif")
-        spaceLabel = tk.Label(self, image = spaceImage)
-        spaceLabel.place(x=0, y=0, relwidth=1, relheight=1)
-        spaceLabel.image = spaceImage
-        
-        spaceLabel.grid_rowconfigure(0, weight=1)
-        spaceLabel.grid_columnconfigure(0, weight=1)
+        self.geometry("800x480")
+
+        container = tk.Label(self)
+        container.place(x=0, y=0, relwidth=1, relheight=1)        
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
 
         self.frames = {}
         for F in (StartPage, SettingsPage, CityPage, CalendarPage):
             page_name = F.__name__
             
-            frame = F(parent = spaceLabel, controller=self)
+            frame = F(parent = container, controller=self)
             
             #spaceImage = tk.PhotoImage(file = "space.gif")
             #frame = tk.Label(self, image = spaceImage)
@@ -77,7 +72,7 @@ class SampleApp(tk.Tk):
 
 class StartPage(tk.Frame):
 
-    def tempDown(self):
+    def tempUp(self):
         if(self.requestedTemperature > 60):
             self.requestedTemperature -= 1
             self.requestedtTempString = str(self.requestedTemperature)+"°F"
@@ -86,7 +81,7 @@ class StartPage(tk.Frame):
         else:
             self.downButton.config(state="disable")
 
-    def tempUp(self):
+    def tempDown(self):
         if(self.requestedTemperature < 90):
             self.requestedTemperature += 1
             self.requestedtTempString = str(self.requestedTemperature)+"°F"
@@ -133,10 +128,10 @@ class StartPage(tk.Frame):
         self.isOn = True
         self.currentTemperature = 80
         self.requestedTemperature = 75
-        spaceImage = tk.PhotoImage(file = "space.gif")
-        spaceLabel = tk.Label(self, image = spaceImage)
-        spaceLabel.place(x=0, y=0, relwidth=1, relheight=1)
-        spaceLabel.image = spaceImage
+        backgroundImage = tk.PhotoImage(file = "bg.png")
+        background = tk.Label(self, image = backgroundImage)
+        background.place(x=0, y=0, relwidth=1, relheight=1)
+        background.image = backgroundImage
         ################################
         #                              #
         #       TIME_WIFI_SYSTEM       #
@@ -145,15 +140,16 @@ class StartPage(tk.Frame):
 
        	wifiImage = tk.PhotoImage(file = "wifi.png")
 	
-        wifiLabel = tk.Label(self, image = wifiImage)
+        wifiLabel = tk.Label(self, image = wifiImage, bg = 'black')
         wifiLabel.place(relx=0.95, rely = 0.01)
         wifiLabel.image = wifiImage
         
 	# Current Time being display top right corner
         now = datetime.datetime.now().time()
 
-        timeLabel = tk.Label(self, text=now.strftime("%I:%M %p"), font=controller.time_font)
-        timeLabel.place(relx = 0.8,rely = 0.02)
+        timeLabel = tk.Label(self, text=now.strftime("%I:%M %p"), font=controller.time_font, bg = 'black', fg = 'white')
+        timeLabel.config(font = ("Calibri", 17))
+        timeLabel.place(relx = 0.55,rely = 0.35)
        
         ################################
         #                              #
@@ -164,9 +160,9 @@ class StartPage(tk.Frame):
 	# Current date that will also be used as a button
 	# displayed top right corner, nect to time
         today = datetime.date.today()
-        calButton = tk.Button(self, text = today.strftime("%m/%d/%y"),relief = 'flat', command=lambda: controller.show_frame("CalendarPage"))
-
-        calButton.place(relx=0.85,rely=0.015)
+        calButton = tk.Button(self, text = today.strftime("%A, %b %d, %Y"), bg = 'black', relief = 'flat', fg = 'white', highlightbackground = 'black', activebackground='#333333', activeforeground = 'white', command=lambda: controller.show_frame("CalendarPage"))
+        calButton.config(font = ("Calibri", 20))
+        calButton.place(relx=0.53,rely=0.25)
 
         ################################
         #                              #
@@ -177,19 +173,19 @@ class StartPage(tk.Frame):
         
         # Power Button
         powerButton = tk.PhotoImage(file = "powerOn.png")
-        self.systemOnOffButton = tk.Button(self, image = powerButton, relief = 'flat', command= self.systemToggle)
+        self.systemOnOffButton = tk.Button(self, image = powerButton, relief = 'flat', command= self.systemToggle, bg = 'black', highlightbackground = 'black',activebackground='#333333', activeforeground = 'white')
         self.systemOnOffButton.image = powerButton
         self.systemOnOffButton.place(relx=.05, rely = 0.01)
         
 	# settings button
         settingsImage = tk.PhotoImage(file = "settings.png")
-        settingsButton = tk.Button(self, image = settingsImage, relief= 'flat',command=lambda: controller.show_frame("SettingsPage"))
+        settingsButton = tk.Button(self, image = settingsImage, relief= 'flat',command=lambda: controller.show_frame("SettingsPage"), bg ='black', highlightbackground = 'black',activebackground='#333333', activeforeground = 'white')
         settingsButton.image = settingsImage
         settingsButton.place(relx=0.1, rely=0.01)
 
 	# vacation button
         vacayImage = tk.PhotoImage(file = "airplaneModeOff.png")
-        self.vacayButton = tk.Button(self, image = vacayImage, relief='flat',command=self.vacayToggle)
+        self.vacayButton = tk.Button(self, image = vacayImage, relief='flat',command=self.vacayToggle, bg = 'black', highlightbackground = 'black',activebackground='#333333', activeforeground = 'white')
         self.vacayButton.image = vacayImage
         self.vacayButton.place(relx=0.15,rely=0.01)
 
@@ -218,7 +214,11 @@ class StartPage(tk.Frame):
         cities = ["Los Angeles,CA", "Las Vegas,NV", "New York,NY","Miami,FL","SEOUL, South Korea","São Paulo, Brazil","Bombay, India", "JAKARTA, Indonesia","Karachi, Pakistan","MOSKVA (Moscow), Russia","Istanbul, Turkey"]
 
         cityOptions = tk.OptionMenu(self, self.variable,*cities ,command=selectCity)
+        cityOptions.configure(fg = 'white', bg = 'black', highlightbackground = 'black',activebackground='#333333', activeforeground = 'white', relief ='flat')
+        cityOptions["menu"].config(fg = 'white', bg = 'black', selectcolor = 'black',activebackground='#333333', activeforeground = 'white')
         cityOptions.place(relx=0.2,rely=0.02)
+
+
         
         # Trying to figure out how to pass data through pages
 #        vacayButton = tk.Button(vccFrame, text="Choose City", command=lambda: controller.show_frame("CityPage"))
@@ -259,7 +259,7 @@ class StartPage(tk.Frame):
         days = [None] * 4
         forecast = threeDayForecast(self.variable.get())
         for i in range(1,4):
-            days[i] = forecast[i].day + " " + forecast[i].date + "\t\t" + forecast[i].high + "°F"
+            days[i] = forecast[i].day + " " + forecast[i].date + "\t" + forecast[i].high + "°F"
             print(locat)
  
         day1TempString = tk.StringVar()
@@ -272,14 +272,14 @@ class StartPage(tk.Frame):
         day3TempString.set(days[3])
 
         # Label for the three day forecast
-        day1TempLabel = tk.Label(self, text=day1TempString.get(), font=controller.day_font)
-        day1TempLabel.place(relx=0.55, rely=0.6)
+        day1TempLabel = tk.Label(self, text=day1TempString.get(), font=controller.day_font, bg = 'black',fg = 'white')
+        day1TempLabel.place(relx=0.55, rely=0.65)
 
-        day2TempLabel = tk.Label(self, text=day2TempString.get(), font=controller.day_font)
-        day2TempLabel.place(relx=0.55, rely=0.67)
+        day2TempLabel = tk.Label(self, text=day2TempString.get(), font=controller.day_font, bg = 'black',fg = 'white')
+        day2TempLabel.place(relx=0.55, rely=0.75)
 
-        day3TempLabel = tk.Label(self, text=day3TempString.get(), font=controller.day_font)
-        day3TempLabel.place(relx=0.55, rely=0.74)
+        day3TempLabel = tk.Label(self, text=day3TempString.get(), font=controller.day_font, bg = 'black',fg = 'white')
+        day3TempLabel.place(relx=0.55, rely=0.85)
 
 
         ################################
@@ -290,13 +290,18 @@ class StartPage(tk.Frame):
         ################################
 
 
+	# outdoor temperature
+
+        outdoorTextLabel = tk.Label(self, text = "Outdoor",font = ("Calibri, 17"),bg = 'black',fg = 'white')
+        outdoorTextLabel.place(relx=0.55, rely=0.5)
         self.currentTempString = str(self.currentTemperature)+"°F"
+        self.currentTempLabel = tk.Label(self, text=self.currentTempString, font=controller.temp_font, bg = 'black',fg = 'white')
+        self.currentTempLabel.config(font = ("Calibri", 17))
+        self.currentTempLabel.place(relx=0.9, rely=0.5)
+
+	# indoor temperature
         self.requestedtTempString = str(self.requestedTemperature)+"°F"
-
-        self.currentTempLabel = tk.Label(self, text=self.currentTempString, font=controller.temp_font)
-        self.currentTempLabel.place(relx=0.55, rely=0.1)
-
-        self.requestedTempLabel = tk.Label(self, text=self.requestedtTempString, font=controller.temp_font)
+        self.requestedTempLabel = tk.Label(self, text=self.requestedtTempString, font=controller.temp_font, bg = 'black',fg = 'white')
         self.requestedTempLabel.place(relx=0.2, rely=0.3)
 
 
@@ -311,11 +316,11 @@ class StartPage(tk.Frame):
         upArrow = tk.PhotoImage(file = "up.png")
         downArrow = tk.PhotoImage(file = "down.png")
 
-        self.upButton = tk.Button(self, image = upArrow, relief = 'flat',command= self.tempUp)
+        self.upButton = tk.Button(self, image = upArrow, relief = 'flat',command= self.tempUp, bg = 'black', highlightbackground = 'black', highlightcolor = 'black',activebackground='#333333', activeforeground = 'white')
         self.upButton.image = upArrow
-        self.upButton.place(relx=0.1, rely =0.7)
-        self.downButton = tk.Button(self, image = downArrow, relief = 'flat', command= self.tempDown)
-        self.downButton.place(relx=0.4, rely=0.7)
+        self.upButton.place(relx=0.09, rely =0.7)
+        self.downButton = tk.Button(self, image = downArrow, relief = 'flat', command= self.tempDown, bg = 'black', highlightbackground = 'black',activebackground='#333333', activeforeground = 'white')
+        self.downButton.place(relx=0.35, rely=0.7)
         self.downButton.image = downArrow
 	
 
